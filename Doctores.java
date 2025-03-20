2e3d608eeff348eb756bc24ce5835951a9e989f5
cristophersantana709 <cristopher.santana@tecnologicoliceocristiano.edu.ec>
2025-03-20 13:12:34 -0500

    Update Doctores.java

--- Doctores.java
+++ Doctores.java
@@ -1,5 +1,36 @@
 package tarea1y2;
+import java.io.Serializable;
+public class Doctores implements Serializable {
+		public String  DoctorID;
+		public String nombres;
+		public String apellidos;
+		public String especialidad;
+		public String telefono;
+		public String Email;
+		
+		public Doctores(String doctorID, String nombres, String apellidos, String especialidad, String telefono,
+				String email) {
+			super();
+			DoctorID = doctorID;
+			this.nombres = nombres;
+			this.apellidos = apellidos;
+			this.especialidad = especialidad;
+			this.telefono = telefono;
+			Email = email;
+		}
 
-public class Doctores {
+		public Doctores() {
+			super();
+		}
 
+
+		public void Imprimir() {
+			System.out.println("DoctoID: "+this.DoctorID);
+			System.out.println("Nombres: "+this.nombres);
+			System.out.println("apellidos: "+this.apellidos);
+			System.out.println("especialidad: "+this.especialidad);
+			System.out.println("telefono: "+this.telefono);
+			System.out.println("Email: " +this.Email);
+		}
 }
+
